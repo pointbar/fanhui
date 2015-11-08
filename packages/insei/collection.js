f6d514faaa1e129dc2bbfe9be6c5b4b94f8108bc
pointbar <stephane.langlois@scopyleft.fr>
2015-11-08 23:42:53 +0100

    Create a Inseis collection

---
+++ packages/insei/collection.js
@@ -0,0 +1,1 @@
+Inseis = new Mongo.Collection('inseis')
--- packages/insei/package.js
+++ packages/insei/package.js
@@ -1,24 +1,22 @@
 Package.describe({
   name: 'pntbr:insei',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'Manage Insei',
+  git: 'https://github.com/goacademie/fanhui',
   documentation: 'README.md'
-});
+})
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('insei.js');
-});
+  api.versionsFrom('1.2.1')
+  api.use(['ecmascript', 'mongo'])
+  api.addFiles('collection.js')
+  api.addFiles(['server/inseiRecords.js', 'server/model.js'], 'server')
+  api.export('Inseis')
+})
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('pntbr:insei');
-  api.addFiles('insei-tests.js');
-});
+  api.use('ecmascript')
+  api.use('tinytest')
+  api.use('pntbr:insei')
+  api.addFiles('insei-tests.js')
+})
