2eb6cb2227c6c45191d3d0e925fc289ad6755156
pointbar <stephane.langlois@scopyleft.fr>
2015-11-10 20:10:21 +0100

    Create files for youtube package

--- packages/youtube/package.js
+++ packages/youtube/package.js
@@ -1,24 +1,30 @@
 Package.describe({
   name: 'pntbr:youtube',
   version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
+  summary: 'Manage Youtube\'s videos',
+  git: 'https://github.com/goacademie/fanhui',
   documentation: 'README.md'
-});
+})
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.2.1');
-  api.use('ecmascript');
-  api.addFiles('youtube.js');
-});
+  api.versionsFrom('1.2.1')
+  api.use(['ecmascript', 'mongo'])
+  api.use(['iron:router', 'templating'], 'client')
+  api.addFiles('collection.js')
+  api.addFiles(['server/vdoRecords.js', 'server/model.js'], 'server')
+  api.addFiles([
+    'client/youtube.html',
+    'client/router.js',
+    'client/list-vdos.html',
+    'client/list-vdos.js',
+    'client/insert-vdo.html',
+    'client/insert-vdo.js'
+  ], 'client')
+})
 
 Package.onTest(function(api) {
-  api.use('ecmascript');
-  api.use('tinytest');
-  api.use('pntbr:youtube');
-  api.addFiles('youtube-tests.js');
-});
+  api.use('ecmascript')
+  api.use('tinytest')
+  api.use('pntbr:youtube')
+  api.addFiles('youtube-tests.js')
+})
