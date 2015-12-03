315e892a8798057256dbe8def1085ceba030afb0
pointbar <stephane.langlois@scopyleft.fr>
2015-12-03 18:57:35 +0100

    Add validation for new youtube vdos

--- packages/youtube/client/insert-vdo.js
+++ packages/youtube/client/insert-vdo.js
@@ -1,39 +1,55 @@
-function callSave(youtubeId) {
-  Meteor.call('save_vdo', youtubeId, (error) => {
-    if (error) {
-      Notifications.warn(
-        `La vidéo : ${youtubeId} - n\'a pas été sauvegardée`, error.reason)
-    } else
-      Notifications.success(`La vidéo : ${youtubeId} - est sauvegardée`)
-  })
-}
+youtubeIdCheckLength = (youtubeId) =>
+  youtubeId.length === 11
 
-function checkYoutubeId(id) {
-  if (id.length !== 11)
-    throw new Meteor.Error('Problème de référence Youtube', 'Une référence comporte 11 signes')
-  Meteor.subscribe('vdos')
-  if (Vdos.find({ youtube_id: youtubeId }).count())
-    throw new Meteor.Error('Problème de référence Youtube', 'La vidéo existe déjà')
-  return true
+notifBadYoutubeId = (youtubeId) =>
+  new Promise((resolve, reject) => {
+    if (! youtubeIdCheckLength(youtubeId)) {
+      Notifications.warn(
+        'Problème de référence Youtube', 'Une référence comporte 11 signes.')
+      reject()
+    } else
+      resolve(youtubeId)
+  })
+
+notifVdoExists = (youtubeId) =>
+  new Promise((resolve, reject) =>
+    Meteor.call('isVdoExists', youtubeId, (err, isExists) => {
+      if (isExists) {
+        Notifications.warn(
+          'Problème de référence Youtube', `${youtubeId} - est déjà présente.`)
+        reject()
+      } else
+        resolve(youtubeId)
+    })
+  )
+
+notifNoYoutubeData = (youtubeId) =>
+  new Promise((resolve, reject) =>
+    Meteor.call('collectYoutubeData', youtubeId, (err, youtubeData) => {
+      if (err) {
+        Notifications.warn(
+          'Problème de référence Youtube', `${err.reason}`)
+        reject()
+      } else
+        resolve(youtubeData)
+    })
+  )
+
+callSave = (youtubeData) => {
+  console.info('data', youtubeData.content)
+}
+checkAndSave = (youtubeId) => {
+  notifBadYoutubeId(youtubeId)
+    .then(notifVdoExists)
+    .then(notifNoYoutubeData)
+    .then(callSave)
 }
 
 Template.insertVdo.events({
   'click #btn_save_vdo': (evt) => {
     let youtubeId = document.querySelector('#input_youtube_id').value
     evt.preventDefault()
-    try {
-      checkYoutubeId(youtubeId)
-    } catch (error) {
-      Notifications.warn(error.error, error.reason)
-      throw new Meteor.Error('bad-youtube-id', 'Bad ID or existant ID')
-    }
-    callSave(youtubeId)
+    checkAndSave(youtubeId)
     document.querySelector('#input_youtube_id').value = ''
   }
 })
-
-function checkLink(youtubeId) {
-  Vdo.find({youtube_id: youtubeId}).count() === 0 ||
-    Session.set('alert', `La vidéo : '${youtubeId}' est déjà présente.`)
-  return ! Session.get('alert')
-}
--- packages/youtube/server/model.js
+++ packages/youtube/server/model.js
@@ -3,11 +3,12 @@ Meteor.publish('vdos', () => {
 })
 
 Meteor.methods({
-  save_vdo: (youtubeId) => {
+  collectYoutubeData: (youtubeId) => {
+    let youtubeData
     try {
-      let result =
+      youtubeData =
         HTTP.get(`http://youtube.com/get_video_info?video_id=${youtubeId}`)
-      if (result.content.match(/errorcode=/))
+      if (youtubeData.content.match(/errorcode=/))
         throw new Meteor.Error('no-yt-video', 'Référence Youtube incorrecte')
     } catch (error) {
       if (error.reason)
@@ -15,56 +16,7 @@ Meteor.methods({
       else
         throw new Meteor.Error(error.Error, 'Impossible d\'accéder à Youtube')
     }
-  }
+    return youtubeData
+  },
+  isVdoExists: (youTubeId) => !! Vdos.find({youtube_id: youTubeId}).count()
 })
-
-function checkTitle(youtubeTitle) {
-  Session.set('alert', false)
-  ;(! youtubeTitle.match(/^\d{3}-(?:Joseki|Fuseki)-\d{2}-\d{2}-\d{4}/)) &&
-    Session.set('alert', `Le titre : '${youtubeTitle}' est incorrecte.`)
-  return ! Session.get('alert')
-}
-
-function getVdoTitle (youtubeId, callback) {
-  Meteor.call('getYoutubeTitle', youtubeId, (error, result) => {
-    let youtubeTitle = result.content.split('&')
-      .filter(parameter => { return (parameter.match('^title=')) })[0]
-      .split('=')[1]
-    checkTitle(youtubeTitle) && callback(youtubeTitle)
-  })
-}
-
-function getTypeByTitle(title) {
-  return title.match(/(?:Joseki)/) && 'Joseki'
-    || title.match(/(?:Fuseki)/) && 'Fuseki'
-}
-
-function getRankByTitle(title) {
-  return +title.match(/^\d{3}/)[0]
-}
-
-function getDateByTitle(title) {
-  let date =  new Date(
-    +title.split('-')[4],
-    +title.split('-')[3] - 1,
-    +title.split('-')[2]
-  )
-  return date
-}
-
-function saveLink(youtubeId) {
-  getVdoTitle(youtubeId, function (youtubeTitle) {
-    let type = getTypeByTitle(youtubeTitle)
-    let rank = getRankByTitle(youtubeTitle)
-    let date = getDateByTitle(youtubeTitle)
-    Vdo.insert({
-      youtube_id: youtubeId,
-      rank: rank,
-      type: type,
-      date: date,
-      youtube_title: youtubeTitle
-    })
-    Session.set('alert',
-      `La video : '${youtubeTitle}' est maintenant disponible.`)
-  })
-}
