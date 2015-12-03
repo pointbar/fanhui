0aeabb3be0404ae606c84c84a749be51c9146350
pointbar <stephane.langlois@scopyleft.fr>
2015-12-03 18:55:04 +0100

    Add unit tests to youtube packages

--- packages/youtube/tests-youtube.js
+++ packages/youtube/tests-youtube.js
@@ -1,4 +1,22 @@
-Tinytest.add('checkYoutubeId', function (test) {
-  console.log('Info')
-  test.equal(true, true)
-})
+if (Meteor.isClient) {
+  Tinytest.add('Check if "1234" is not correct.', (test) => {
+    test.isFalse(youtubeIdCheckLength(1234))
+  })
+  Tinytest.addAsync('Check the "AxRg12345tG" reférence exists in our collection.',
+    (test, next) => {
+      Meteor.call('isVdoExists', 'AxRg12345tG', (err, result) => {
+        test.isTrue(result)
+        next()
+      })
+    })
+  Tinytest.addAsync('Check the "12345678901" reférence don\'t exists in collection.',
+    (test, next) => {
+      Meteor.call('isVdoExists', '12345678901', (err, result) => {
+        test.isFalse(result)
+        next()
+      })
+    })
+}
+
+if (Meteor.isServer) {
+}
