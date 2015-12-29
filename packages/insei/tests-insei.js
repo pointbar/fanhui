d78e88af5458398d4b503170b59d9a9807de4d01
pointbar <stephane.langlois@scopyleft.fr>
2015-12-29 22:44:12 +0100

    Checked is an insei or a sensei

--- packages/insei/tests-insei.js
+++ packages/insei/tests-insei.js
@@ -1,3 +1,6 @@
-if (Meteor.isClient) {
-
-}
+Tinytest.add('Check if pntbr is an Insei', (test) =>
+  test.isTrue(isInsei('pntbr'))
+)
+Tinytest.add('Check if oragefan is a sensei', (test) =>
+  test.isTrue(isSensei('oragefan'))
+)
