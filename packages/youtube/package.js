83e030e1c24cb907fb931e7ab9813a13c8c19672
pointbar <stephane.langlois@scopyleft.fr>
2016-01-01 01:03:34 +0100

    Fix delete unecessary file in package

--- packages/youtube/package.js
+++ packages/youtube/package.js
@@ -10,7 +10,6 @@ Package.onUse(function(api) {
   api.use(['ecmascript'])
   api.use(['iron:router', 'templating'], 'client')
   api.addFiles([
-    'client/FO-youtube.html',
     'client/FO-player-vdo.html',
     'client/style.css',
     'client/router.js'
