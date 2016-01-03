4cd49d66de1be3c5683f58861d7d9a931cb5710d
pointbar <stephane.langlois@scopyleft.fr>
2016-01-03 02:13:10 +0100

    Adpat package setting

---
+++ packages/slack-inseis/package.js
@@ -0,0 +1,39 @@
+Package.describe({
+  name: 'pntbr:slack-inseis',
+  version: '0.0.1',
+  summary: 'Manage Inseis',
+  git: 'https://github.com/goacademie/fanhui',
+  documentation: 'README.md'
+})
+Package.onUse(function(api) {
+  api.versionsFrom('1.2.1')
+  api.use(['ecmascript', 'mongo'])
+  api.use('hiukim:slack-client', 'server')
+  api.use(['iron:router', 'templating'], 'client')
+  api.addFiles('collection.js')
+  api.addFiles([
+    'server/slack-inseis.js',
+    'server/slack-token.js',
+    'server/record-slack-inseis.js',
+    'server/model.js'
+  ], 'server')
+  api.addFiles([
+    'client/router.js',
+    'client/inseis.js',
+    'client/list-inseis.html',
+    'client/list-inseis.js'
+  ], 'client')
+  api.export(['SlackInseis', 'Inseis'], 'server')
+  api.export([
+    'isInsei',
+    'isSensei',
+    'getInseiBySlackName',
+    'nickSlackFromUrl'
+  ], 'client')
+})
+Package.onTest(function(api) {
+  api.use(['ecmascript', 'tinytest'])
+  api.use(['iron:router@1.0.0', 'templating'], 'client')
+  api.use('pntbr:slack-inseis')
+  api.addFiles(['tests-stubs.js', 'tests-insei.js'])
+})
