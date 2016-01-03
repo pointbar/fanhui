8a59e6b25ea6ccbc60f2bd3519ccbb7e62d59d1e
pointbar <stephane.langlois@scopyleft.fr>
2016-01-03 02:02:14 +0100

    Rename insei package in slack-inseis

--- packages/insei/client/list-inseis.js
+++
@@ -1,5 +0,0 @@
-Template.inseisList.helpers({
-  inseis: () => Inseis.find(
-    {role: 'insei'},
-    {sort: {slack_lastName: 1}})
-})
--- packages/insei/server/slack-token-sample.js
+++
@@ -1,5 +0,0 @@
-/*
-  Add a bot at https://my.slack.com/services/new/bot and copy the token beneath.
-  Rename this file: slack-token.js
-*/
-slackAuthToken = 'xox-...'
